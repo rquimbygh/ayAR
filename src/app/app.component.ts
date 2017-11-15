04c330eb6a03de9213f1c0d023893905477a8b5c
rquimby <rquimby600@gmail.com>
2017-11-15 09:20:27 -0500

    Add app template file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'a-root',
-  template: `
-    <a-pattern-marker model='sphere'></a-pattern-marker>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
